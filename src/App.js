2702f15733375f72d93b4ddfc509bbc2beda8f11
rodericasm <rodericasm@gmail.com>
2020-10-15 01:34:45 +1000

    start edit functions

--- src/App.js
+++ src/App.js
@@ -67,14 +67,17 @@ export default function App() {
   return (
     <div className="App">
       <div className="toolbar">
-      <div className="left_nav">{mode}</div>
       {mode == "edit" ?
-             <div className="right_nav">
-             <button onClick={() => set_mode("draw")}>draw</button>
+             <div className="center_nav">
+<svg onClick={() => set_mode("draw")} width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-file-play" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M4 0h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H4z"/>
+  <path d="M6 10.117V5.883a.5.5 0 0 1 .757-.429l3.528 2.117a.5.5 0 0 1 0 .858l-3.528 2.117a.5.5 0 0 1-.757-.43z"/>
+</svg>
+ 
                  </div> 
                  :
-                     <div className="right_nav">
-                       <svg onClick={() => set_mode("edit")} width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                     <div className="center_nav">
+                                           <svg onClick={() => set_mode("edit")} width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                </svg>
@@ -87,7 +90,7 @@ export default function App() {
       </div>
 
       <div className="text_container" id="text_container">
-  <div className="text_area" id="text_area" lang="en" contenteditable="true" data-text="Enter text here!"></div>
+  <div className="text_area" id="text_area" lang="en" contenteditable="true" data-text="Enter text here!" spellcheck="false"></div>
 </div>
 <canvas id="myCanvas" resize="true"></canvas>
 
