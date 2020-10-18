0594b3386862e02f193d7020a3cc7baa06a088be
rodericasm <rodericasm@gmail.com>
2020-10-18 20:29:58 +1000

    travis

--- public/paper.js
+++ public/paper.js
@@ -2,7 +2,8 @@
 // before firing the next onMouseDrag event:
 
 var path;
-console.log("helloz");
+var path_buffer = [];
+var path_history = [];
 function onMouseDown(event) {
   // Create a new path and give it a stroke color:
   path = new Path();
@@ -10,22 +11,39 @@ function onMouseDown(event) {
 
   // Add a segment to the path where
   // you clicked:
+  path_buffer.push(path);
   path.add(event.point);
 }
 
 function onMouseDrag(event) {
   // Every drag event, add a segment
   // to the path at the position of the mouse:
+  path_buffer.push(path);
   path.add(event.point);
 }
 
-function onFrame(event) {
+function onMouseUp(event) {
+  
+  path_history.push(path_buffer);
+  path_buffer = [];
+  console.log(path_history[0]);
 }
 
 window.paperscript_ready = true;
 
 window.clear_canvas = function () {
   project.clear();
+  path = null;
+  path_buffer = [];
+  path_history = [];
+}
+
+window.undo_draw = function () {
+
+}
+
+window.redo_draw = function () {
+  
 }
 
 window.resize_paper = function(w, h) {
