c7bee1960aab26a7f454cde9d9880fef6c877883
rodericasm <rodericasm@gmail.com>
2020-10-18 20:41:48 +1000

    removed warnings and errors

--- src/App.js
+++ src/App.js
@@ -8,18 +8,19 @@ export default function App() {
 
 
   useEffect(() => {
+    var my_canvas = document.getElementById("myCanvas");
+    var text_container = document.getElementById("text_container");
+    var text_area = document.getElementById("text_area"); 
+    var cur_canv_width = my_canvas.style.width;
     switch (mode) {
       case "init":
-        var my_canvas = document.getElementById("myCanvas");
-        var text_container = document.getElementById("text_container");
-        var text_area = document.getElementById("text_area"); 
         var loading_screen = document.getElementById("loading_screen");
         loading_screen.style.display = "none";
         text_area.setAttribute("data-text", "");
         text_area.innerHTML = "Welcome to Quick Annotate! This is a sample text, draw and annotate around me. Press the button above to edit me!"
         text_container.style.height = text_container.getBoundingClientRect().height + "px";
         text_container.style.width = text_container.getBoundingClientRect().width + "px";
-        my_canvas.style.width = my_canvas.style.width;
+        my_canvas.style.width = cur_canv_width;
         my_canvas.style.pointerEvents = "auto";
         text_container.style.pointerEvents = "none";
         
@@ -34,13 +35,10 @@ export default function App() {
         }
         break;
       case "draw":
-        var my_canvas = document.getElementById("myCanvas");
-        var text_container = document.getElementById("text_container");
-        var text_area = document.getElementById("text_area");
         text_area.setAttribute("data-text", "");
         text_container.style.height = text_container.getBoundingClientRect().height + "px";
         text_container.style.width = text_container.getBoundingClientRect().width + "px";
-        my_canvas.style.width = my_canvas.style.width;
+        my_canvas.style.width = cur_canv_width;
         my_canvas.style.pointerEvents = "auto";
         text_container.style.pointerEvents = "none";
         text_area.style.borderBottom = "";
@@ -58,9 +56,7 @@ export default function App() {
         
         break;
       case "edit":
-        var my_canvas = document.getElementById("myCanvas");
-        var text_container = document.getElementById("text_container");
-        var text_area = document.getElementById("text_area");
+
         window.clear_canvas();
         text_area.innerHTML = "";
         text_area.setAttribute("data-text", "Enter text here. Then press the button above to draw and annotate!");
@@ -79,11 +75,6 @@ export default function App() {
  
   }, [mode]); 
 
- 
-  const edit_state = () => {
-    window.location.reload();
-  }
-
   useEffect(() => {
     const interval = setInterval(() => {
       if (window.paperscript_ready){
@@ -99,10 +90,10 @@ export default function App() {
         <p>Loading Quick Annotate....</p>
       </div>
       <div className="toolbar">
-      {mode == "edit" ?
+      {mode === "edit" ?
              <div className="center_nav">
-                  <svg onClick={() => set_mode("draw")} width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-file-play" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M4 0h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H4z"/>
+                  <svg onClick={() => set_mode("draw")} width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-file-play" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" d="M4 0h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H4z"/>
                     <path d="M6 10.117V5.883a.5.5 0 0 1 .757-.429l3.528 2.117a.5.5 0 0 1 0 .858l-3.528 2.117a.5.5 0 0 1-.757-.43z"/>
                   </svg>
 
@@ -110,8 +101,8 @@ export default function App() {
               </div> 
                  :
                      <div className="center_nav">
-                       <svg onClick={() => set_mode("edit")} width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                          <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                       <svg onClick={() => set_mode("edit")} width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                          <path fillRule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                        </svg>
 
                          </div> 
@@ -123,7 +114,7 @@ export default function App() {
       </div>
 
       <div className="text_container" id="text_container">
-  <div className="text_area" id="text_area" lang="en" contenteditable="true" data-text="" spellcheck="false"></div>
+  <div className="text_area" id="text_area" lang="en" contentEditable="true" data-text="" spellCheck="false"></div>
 </div>
 <canvas id="myCanvas" resize="true"></canvas>
 
